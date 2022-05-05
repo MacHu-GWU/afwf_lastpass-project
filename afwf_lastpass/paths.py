ac866de7b1530eb6177a7c1516e2aa836c02c210
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-05-05 17:30:59 -0400

    use ~/.alfred-afwf_lastpass to store config info

--- afwf_lastpass/paths.py
+++ afwf_lastpass/paths.py
@@ -7,12 +7,13 @@ dir_home = Path.home()
 dir_alfred_workflow = Path(dir_home, ".alfred-afwf_lastpass")
 dir_cache = Path(dir_alfred_workflow, ".cache")
 dir_name_index = Path(dir_alfred_workflow, "name_index")
+path_name_txt = Path(dir_alfred_workflow, "name.txt")
 path_python_interpreter = Path(dir_alfred_workflow, "python_interpreter")
 
 dir_here = Path.dir_here(__file__)
 dir_project_root = dir_here.parent
 dir_cache_for_test = Path(dir_project_root, "tests", ".cache")
 dir_name_index_for_test = Path(dir_project_root, "tests", "name_index")
-path_name_json_for_test = Path(dir_here, "tests", "repos.json")
+path_name_txt_for_test = Path(dir_project_root, "tests", "name.txt")
 
 dir_alfred_workflow.mkdir_if_not_exists()
