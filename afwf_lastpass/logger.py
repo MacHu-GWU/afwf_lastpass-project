943a2231a0173c7f19dde391d08a7ff594a97478
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-05-06 21:55:24 -0400

    Create logger.py

---
+++ afwf_lastpass/logger.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from loguru import logger
+from .paths import path_log_txt
+
+logger.remove()
+logger.add(sys.stdout)
+logger.add(path_log_txt.abspath, rotation="10 MB", retention="30 days")
